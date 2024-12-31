96be47bb96174af2c17a0f3b0395f4ff1541a7d7
dewanakl <dewanakretarta29@gmail.com>
2024-12-31 22:35:29 +0700

    refactor: remove unused audio import

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 import { util } from './util.js';
 import { guest } from './guest.js';
 import { theme } from './theme.js';
-import { audio } from './audio.js';
 import { comment } from './comment.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,6 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.util = util;
     window.guest = guest;
     window.theme = theme;
-    window.audio = audio;
     window.comment = comment;
 });
